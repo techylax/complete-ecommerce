9c02901ee8613297ac96ea14fb6a3224448331b9
Laxman Poudel <coderlaxman@gmail.com>
2022-01-14 22:30:35 -0500

    Update

---
+++ core/forms.py
@@ -0,0 +1,42 @@
+import phonenumbers
+from django import forms
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+PAYMENT_CHOICES = (
+    ('P', 'Cash on Delivery')
+)
+
+
+class CheckoutForm(forms.Form):
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Address',
+        'class': 'form-control'
+    }))
+
+    country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
+        'class': 'custom-select d-block w-100'
+
+    }))
+    zip = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    # same_shipping_address = forms.BooleanField(required=False)
+    # save_info = forms.BooleanField(required=False)
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+
+class CouponForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Promo code'
+    }))
+
+
+class RefundForm(forms.Form):
+    ref_code = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'rows': 4
+    }))
+    email = forms.EmailField()
